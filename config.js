836087a65ce50350df3e1b710c4d0e88ebbc8bdb
Nguyenletrungthien <101155715+Nguyenletrungthien@users.noreply.github.com>
2022-11-04 07:54:04 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Bé';
+const giftUrl = 'https://www.facebook.com/hitran3112';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng Bé Trân';
+const contentCard = 'Chúc mừng sinh nhật pé! Chúc pé nhan sắc "quyết liệt" thăng hoa, tiền tài ào ào thăng tiến, và tình yêu "tưng bưng" bùng nổ nhé!';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
